adff5ab62df29af58ca56405ea0aa2bd8ae05a4c
kim-youngchul <kmosh1212@naver.com>
2024-11-22 02:22:21 +0900

    feat: BaseEntity, SoftDeleteEntity 각각 getter 삽입

--- src/main/java/net/pointofviews/common/domain/BaseEntity.java
+++ src/main/java/net/pointofviews/common/domain/BaseEntity.java
@@ -1,10 +1,12 @@
 package net.pointofviews.common.domain;
 
 import jakarta.persistence.MappedSuperclass;
+import lombok.Getter;
 import org.springframework.data.annotation.CreatedDate;
 
 import java.time.LocalDateTime;
 
+@Getter
 @MappedSuperclass
 public abstract class BaseEntity {
 
--- src/main/java/net/pointofviews/common/domain/SoftDeleteEntity.java
+++ src/main/java/net/pointofviews/common/domain/SoftDeleteEntity.java
@@ -1,7 +1,10 @@
 package net.pointofviews.common.domain;
 
+import lombok.Getter;
+
 import java.time.LocalDateTime;
 
+@Getter
 public abstract class SoftDeleteEntity extends BaseEntity {
 
     private LocalDateTime deletedAt;
