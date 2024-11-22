1f1659453bf9017135deab8abad22f97d17f8029
kim-youngchul <kmosh1212@naver.com>
2024-11-22 12:08:19 +0900

    test: Club 생성자, 빌더, maxParticipants 범위 제한 로직 및 테스트 작성

--- src/main/java/net/pointofviews/club/domain/Club.java
+++ src/main/java/net/pointofviews/club/domain/Club.java
@@ -4,15 +4,21 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 import net.pointofviews.common.domain.BaseEntity;
 
 import java.util.UUID;
 
 @Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Club extends BaseEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.UUID)
-    public UUID id;
+    private UUID id;
 
     private String name;
 
@@ -21,4 +27,19 @@ public class Club extends BaseEntity {
     private Integer maxParticipants;
 
     private boolean isPublic;
+
+    @Builder
+    private Club(String description, boolean isPublic, Integer maxParticipants, String name) {
+        this.maxParticipants = validateMaxParticipants(maxParticipants);
+        this.description = description;
+        this.isPublic = isPublic;
+        this.name = name;
+    }
+
+    private Integer validateMaxParticipants(Integer maxParticipants) {
+        if (maxParticipants == null || maxParticipants < 1 || maxParticipants > 1000) {
+            return 1000;
+        }
+        return maxParticipants;
+    }
 }
