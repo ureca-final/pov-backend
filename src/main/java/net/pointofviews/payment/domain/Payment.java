ede1b6ea0212635cb8a5d6a3f972fa9c4787f8fb
ssiiin0-0 <gomee312@naver.com>
2024-11-22 11:22:19 +0900

    feat: Payment 생성자, 빌더, 테스트 작성

--- src/main/java/net/pointofviews/payment/domain/Payment.java
+++ src/main/java/net/pointofviews/payment/domain/Payment.java
@@ -1,10 +1,16 @@
 package net.pointofviews.payment.domain;
 
 import jakarta.persistence.*;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 import net.pointofviews.common.domain.BaseEntity;
 import net.pointofviews.member.domain.Member;
+import org.springframework.util.Assert;
 
 @Entity
+@Getter
+@NoArgsConstructor
 public class Payment extends BaseEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -16,4 +22,15 @@ public class Payment extends BaseEntity {
     private String paymentKey;
     private String vendor;
     private Integer price;
+
+
+    @Builder
+    public Payment(Member member, String paymentKey, String vendor, Integer price) {
+        Assert.notNull(paymentKey, "Payment key must not be null");
+
+        this.member = member;
+        this.paymentKey = paymentKey;
+        this.vendor = vendor;
+        this.price = price;
+    }
 }
