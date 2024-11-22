b3af7fb34fa0f9acc37ad2bd0e0a3439767e1f69
kim-youngchul <kmosh1212@naver.com>
2024-11-22 12:07:30 +0900

    test: ClubMovie 생성자 작성 및 테스트

--- src/main/java/net/pointofviews/club/domain/ClubMovie.java
+++ src/main/java/net/pointofviews/club/domain/ClubMovie.java
@@ -4,10 +4,15 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.FetchType;
 import jakarta.persistence.Id;
 import jakarta.persistence.ManyToOne;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 import net.pointofviews.common.domain.BaseEntity;
 import net.pointofviews.movie.domain.Movie;
 
 @Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class ClubMovie extends BaseEntity {
     @Id
     private Long id;
@@ -17,4 +22,9 @@ public class ClubMovie extends BaseEntity {
 
     @ManyToOne(fetch = FetchType.LAZY)
     private Club club;
+
+    public ClubMovie(Club club, Movie movie) {
+        this.club = club;
+        this.movie = movie;
+    }
 }
---
+++ src/test/java/net/pointofviews/club/domain/ClubMovieTest.java
@@ -0,0 +1,39 @@
+package net.pointofviews.club.domain;
+
+import net.pointofviews.movie.domain.Movie;
+import org.assertj.core.api.SoftAssertions;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import static org.mockito.Mockito.mock;
+
+@ExtendWith(MockitoExtension.class)
+class ClubMovieTest {
+
+    @Nested
+    class Constructor {
+
+        @Nested
+        class Success {
+
+            @Test
+            void ClubMovie_객체_생성() {
+                // given
+                Club club = mock(Club.class);
+                Movie movie = mock(Movie.class);
+
+                // when
+                ClubMovie clubMovie = new ClubMovie(club, movie);
+
+                // then
+                SoftAssertions.assertSoftly(softly -> {
+                    softly.assertThat(clubMovie.getClub()).isEqualTo(club);
+                    softly.assertThat(clubMovie.getMovie()).isEqualTo(movie);
+                });
+            }
+        }
+    }
+
+}
