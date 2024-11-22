674c0cad00ac001ca69b01b70987e7a51b038658
ssiiin0-0 <gomee312@naver.com>
2024-11-22 16:57:11 +0900

    feat: ReviewLike 생성자, 빌더, 테스트 작성

---
+++ src/test/java/net/pointofviews/review/domain/ReviewLikeTest.java
@@ -0,0 +1,43 @@
+package net.pointofviews.review.domain;
+
+import net.pointofviews.member.domain.Member;
+import org.assertj.core.api.SoftAssertions;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import static org.mockito.Mockito.mock;
+
+@ExtendWith(MockitoExtension.class)
+class ReviewLikeTest {
+
+    @Nested
+    class Constructor {
+
+        @Nested
+        class success {
+
+            @Test
+            void ReviewLike_객체_생성() {
+                // given
+                Member member = mock(Member.class);
+                Review review = mock(Review.class);
+
+                // when
+                ReviewLike reviewLike = ReviewLike.builder()
+                        .member(member)
+                        .review(review)
+                        .build();
+
+                // then
+                SoftAssertions.assertSoftly(softly -> {
+                    softly.assertThat(reviewLike).isNotNull();
+                    softly.assertThat(reviewLike.getMember()).isEqualTo(member);
+                    softly.assertThat(reviewLike.getReview()).isEqualTo(review);
+                    softly.assertThat(reviewLike.getCreatedAt()).isNull();
+                });
+            }
+        }
+    }
+}
