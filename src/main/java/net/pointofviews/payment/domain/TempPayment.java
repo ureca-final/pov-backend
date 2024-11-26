dc76079a59954563a26dd211dc922bb5d66b813b
ssiiin0-0 <gomee312@naver.com>
2024-11-26 18:14:55 +0900

    feat: Payment관련 TempPayment 엔티티 및 테스트 코드 추가

---
+++ src/main/java/net/pointofviews/payment/domain/TempPayment.java
@@ -0,0 +1,38 @@
+package net.pointofviews.payment.domain;
+
+import jakarta.persistence.*;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import net.pointofviews.common.domain.BaseEntity;
+import net.pointofviews.member.domain.Member;
+import org.springframework.util.Assert;
+
+@Entity
+@Getter
+@NoArgsConstructor
+public class TempPayment extends BaseEntity{
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @OneToOne
+    private Member member;
+
+    private String orderId;
+
+    @Enumerated(EnumType.STRING)
+    private OrderType type;
+
+    private Integer amount;
+
+    @Builder
+    public TempPayment( Member member, String orderId, OrderType type, Integer amount) {
+        Assert.notNull(orderId, "Order ID must not be null");
+
+        this.member = member;
+        this.orderId = orderId;
+        this.type = type;
+        this.amount = amount;
+    }
+}
