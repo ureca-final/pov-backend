5fc230135cb0bb1999fa2208fe7e6b1e2deb5e51
ssiiin0-0 <gomee312@naver.com>
2024-11-26 19:07:29 +0900

    refactor: TempPayment 내 access level 추가

--- src/main/java/net/pointofviews/payment/domain/TempPayment.java
+++ src/main/java/net/pointofviews/payment/domain/TempPayment.java
@@ -1,6 +1,7 @@
 package net.pointofviews.payment.domain;
 
 import jakarta.persistence.*;
+import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -10,7 +11,7 @@ import org.springframework.util.Assert;
 
 @Entity
 @Getter
-@NoArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class TempPayment extends BaseEntity{
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
