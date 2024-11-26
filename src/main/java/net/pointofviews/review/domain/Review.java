c6a1e5d2a4f589d804e0aa2a3ec0f07940116279
leeseunghee00 <lsh9246@gmail.com>
2024-11-26 14:49:18 +0900

    feat: Review Entity preference 필드 추가

--- src/main/java/net/pointofviews/review/domain/Review.java
+++ src/main/java/net/pointofviews/review/domain/Review.java
@@ -1,16 +1,24 @@
 package net.pointofviews.review.domain;
 
-import jakarta.persistence.*;
+import java.time.LocalDateTime;
+
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.util.Assert;
+
+import net.pointofviews.common.domain.SoftDeleteEntity;
+import net.pointofviews.member.domain.Member;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
 import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import net.pointofviews.common.domain.SoftDeleteEntity;
-import net.pointofviews.member.domain.Member;
-import org.springframework.data.annotation.LastModifiedDate;
-import org.springframework.util.Assert;
-
-import java.time.LocalDateTime;
 
 @Entity
 @Getter
@@ -27,6 +35,8 @@ public class Review extends SoftDeleteEntity {
 
     @Column(columnDefinition = "TEXT")
     private String contents;
+
+    private String preference;
 
     private boolean isSpoiler;
 
