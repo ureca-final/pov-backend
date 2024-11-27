484bc258e2705a63349c028055bd2e948b9e2c17
kim-youngchul <kmosh1212@naver.com>
2024-11-27 10:52:20 +0900

    feat: 400 exception 생성

---
+++ src/main/java/net/pointofviews/common/exception/BadRequestException.java
@@ -0,0 +1,9 @@
+package net.pointofviews.common.exception;
+
+import org.springframework.http.HttpStatus;
+
+public class BadRequestException extends BusinessException {
+    public BadRequestException(String message) {
+        super(HttpStatus.BAD_REQUEST, message);
+    }
+}
