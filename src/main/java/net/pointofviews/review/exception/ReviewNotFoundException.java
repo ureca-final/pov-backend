460ec51204b4ad2f031c0d13e568d4f646eab6af
ssiiin0-0 <gomee312@naver.com>
2024-11-27 14:55:29 +0900

    feat: review keyword관련 repository 추가

---
+++ src/main/java/net/pointofviews/review/exception/ReviewNotFoundException.java
@@ -0,0 +1,11 @@
+package net.pointofviews.review.exception;
+
+import org.springframework.http.HttpStatus;
+import net.pointofviews.common.exception.BusinessException;
+
+public class ReviewNotFoundException extends BusinessException {
+
+  public ReviewNotFoundException() {
+    super(HttpStatus.NOT_FOUND, "존재하지 않는 리뷰입니다.");
+  }
+}
