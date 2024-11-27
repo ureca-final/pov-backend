95056b26d6a619233bb4a68a3a85a093d1a70cb3
leeseunghee00 <lsh9246@gmail.com>
2024-11-27 22:18:00 +0900

    refactor: FindReviewByMovie 실패 테스트 수정

--- src/test/java/net/pointofviews/review/service/ReviewServiceTest.java
+++ src/test/java/net/pointofviews/review/service/ReviewServiceTest.java
@@ -17,10 +17,11 @@ import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Slice;
 import org.springframework.data.domain.SliceImpl;
+import org.springframework.http.HttpStatus;
 
 import net.pointofviews.member.domain.Member;
 import net.pointofviews.movie.domain.Movie;
-import net.pointofviews.movie.exception.MovieNotFoundException;
+import net.pointofviews.movie.exception.MovieException;
 import net.pointofviews.movie.repository.MovieRepository;
 import net.pointofviews.review.domain.Review;
 import net.pointofviews.review.dto.response.ReadReviewListResponse;
@@ -111,15 +112,19 @@ class ReviewServiceTest {
 		class Failure {
 
 			@Test
-			void 존재하지_않는_영화_MovieNotFoundException_예외발생() {
+			void 존재하지_않는_영화_MovieException_movieNotFound_예외발생() {
 			    // given -- 테스트의 상태 설정
 				given(movieRepository.findById(any())).willReturn(Optional.empty());
 
 			    // when -- 테스트하고자 하는 행동
+				MovieException exception = assertThrows(MovieException.class, () ->
+					reviewService.findReviewByMovie(1L, PageRequest.of(0, 10))
+				);
 
 			    // then -- 예상되는 변화 및 결과
-				assertThrows(MovieNotFoundException.class, () -> {
-					reviewService.findReviewByMovie(1L, PageRequest.of(0, 10));
+				assertSoftly(softly -> {
+					softly.assertThat(exception.getStatus()).isEqualTo(HttpStatus.NOT_FOUND);
+					softly.assertThat(exception.getMessage()).isEqualTo("영화(Id: 1)는 존재하지 않습니다.");
 				});
 			}
 
