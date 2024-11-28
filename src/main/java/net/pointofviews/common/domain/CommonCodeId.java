1213a592f93d6c36dfe9307c8ec9ffe12ac27ec1
leeseunghee00 <lsh9246@gmail.com>
2024-11-29 00:27:34 +0900

    refactor: CommonCodeTest 수정

--- src/main/java/net/pointofviews/common/domain/CommonCodeId.java
+++ src/main/java/net/pointofviews/common/domain/CommonCodeId.java
@@ -5,9 +5,11 @@ import java.io.Serializable;
 import jakarta.persistence.Embeddable;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Embeddable
+@Getter
 @AllArgsConstructor
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class CommonCodeId implements Serializable {
