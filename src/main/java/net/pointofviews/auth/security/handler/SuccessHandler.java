bc856103c1615609f93d3a12440800a98fc1055f
ssiiin0-0 <gomee312@naver.com>
2024-11-29 17:12:28 +0900

    refactor: OAuth 관련 코드 제거

--- src/main/java/net/pointofviews/auth/security/handler/SuccessHandler.java
+++ src/main/java/net/pointofviews/auth/security/handler/SuccessHandler.java
@@ -5,14 +5,14 @@ import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import net.pointofviews.auth.utils.JwtProvider;
 import org.springframework.security.core.Authentication;
-import org.springframework.security.web.authentication.SimpleUrlAuthenticationSuccessHandler;
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
 import org.springframework.stereotype.Component;
 
 import java.io.IOException;
 
 @Component
 @RequiredArgsConstructor
-public class OAuth2SuccessHandler extends SimpleUrlAuthenticationSuccessHandler {
+public class SuccessHandler implements AuthenticationSuccessHandler {
     private final JwtProvider jwtProvider;
 
     @Override
