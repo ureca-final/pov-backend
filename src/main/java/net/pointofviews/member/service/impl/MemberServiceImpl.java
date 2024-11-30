ba5d998bffb6c27a9ab749d8e3feed260696a640
leeseunghee00 <lsh9246@gmail.com>
2024-11-30 18:25:34 +0900

    feat: 회원 닉네임 변경 구현

--- src/main/java/net/pointofviews/member/service/impl/MemberServiceImpl.java
+++ src/main/java/net/pointofviews/member/service/impl/MemberServiceImpl.java
@@ -1,23 +1,33 @@
 package net.pointofviews.member.service.impl;
 
-import lombok.RequiredArgsConstructor;
+import static net.pointofviews.member.exception.MemberException.*;
+
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
 import net.pointofviews.auth.dto.request.CreateMemberRequest;
 import net.pointofviews.auth.dto.request.LoginMemberRequest;
 import net.pointofviews.auth.dto.response.CreateMemberResponse;
 import net.pointofviews.auth.dto.response.LoginMemberResponse;
+import net.pointofviews.member.dto.request.PutMemberGenreListRequest;
+import net.pointofviews.member.dto.request.PutMemberImageRequest;
+import net.pointofviews.member.dto.request.PutMemberNicknameRequest;
+import net.pointofviews.member.dto.request.PutMemberNoticeRequest;
+import net.pointofviews.member.dto.response.PutMemberGenreListResponse;
+import net.pointofviews.member.dto.response.PutMemberImageResponse;
+import net.pointofviews.member.dto.response.PutMemberNicknameResponse;
+import net.pointofviews.member.dto.response.PutMemberNoticeResponse;
 import net.pointofviews.auth.utils.JwtProvider;
 import net.pointofviews.member.domain.Member;
-import net.pointofviews.member.dto.request.*;
-import net.pointofviews.member.dto.response.*;
+import net.pointofviews.member.exception.MemberException;
 import net.pointofviews.member.repository.MemberRepository;
 import net.pointofviews.member.service.MemberService;
-import org.springframework.stereotype.Service;
 
-import static net.pointofviews.member.exception.MemberException.invalidSocialType;
-import static net.pointofviews.member.exception.MemberException.memberNotFound;
+import lombok.RequiredArgsConstructor;
 
 @Service
 @RequiredArgsConstructor
+@Transactional(readOnly = true)
 public class MemberServiceImpl implements MemberService {
 
     private final MemberRepository memberRepository;
@@ -32,7 +42,7 @@ public class MemberServiceImpl implements MemberService {
     public LoginMemberResponse login(LoginMemberRequest request) {
         // 이메일로 회원 조회
         Member member = memberRepository.findByEmail(request.email())
-                .orElseThrow(() -> memberNotFound());
+                .orElseThrow(MemberException::memberNotFound);
 
         if (!member.getSocialType().name().equals(request.socialType())) {
             throw invalidSocialType();
@@ -63,8 +73,19 @@ public class MemberServiceImpl implements MemberService {
     }
 
     @Override
-    public PutMemberNicknameResponse updateNickname(PutMemberNicknameRequest request) {
-        return null;
+    @Transactional
+    public PutMemberNicknameResponse updateNickname(Member loginMember, PutMemberNicknameRequest request) {
+
+        Member member = memberRepository.findById(loginMember.getId())
+            .orElseThrow(() -> memberNotFound(loginMember.getId()));
+
+        if (memberRepository.existsByNickname(request.nickname())) {
+            throw nicknameDuplicate();
+        }
+
+        member.updateNickname(request.nickname());
+
+        return new PutMemberNicknameResponse(member.getNickname());
     }
 
     @Override
