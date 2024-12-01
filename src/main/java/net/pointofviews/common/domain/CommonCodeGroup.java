7feae646720c41c2bdada3a1beed861bf5f4ff17
kim-youngchul <kmosh1212@naver.com>
2024-12-02 05:05:49 +0900

    refactor: CommonCodeGroup 생성 방식 변경, hash collection 사용으로 인한 Hashcode, Equals 메서드 작성, 로깅용 toString 작성

--- src/main/java/net/pointofviews/common/domain/CommonCodeGroup.java
+++ src/main/java/net/pointofviews/common/domain/CommonCodeGroup.java
@@ -8,6 +8,8 @@ import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
+import java.util.Objects;
+
 @Entity
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
@@ -26,10 +28,38 @@ public class CommonCodeGroup {
     private boolean isActive;
 
     @Builder
-    private CommonCodeGroup(String groupCode, String name, String description) {
-        this.groupCode = groupCode;
-        this.name = name;
-        this.description = description;
-        this.isActive = true;
+    private CommonCodeGroup(CodeGroupEnum codeGroupEnum) {
+        this.groupCode = codeGroupEnum.getCode();
+        this.name = codeGroupEnum.getName();
+        this.description = codeGroupEnum.getDescription();
+        this.isActive = codeGroupEnum.isActive();
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        CommonCodeGroup codeGroup = (CommonCodeGroup) o;
+        return isActive() == codeGroup.isActive() && Objects.equals(getGroupCode(), codeGroup.getGroupCode()) && Objects.equals(getName(), codeGroup.getName()) && Objects.equals(getDescription(), codeGroup.getDescription());
+    }
+
+    @Override
+    public int hashCode() {
+        int result = Objects.hashCode(getGroupCode());
+        result = 31 * result + Objects.hashCode(getName());
+        result = 31 * result + Objects.hashCode(getDescription());
+        result = 31 * result + Boolean.hashCode(isActive());
+        return result;
+    }
+
+    @Override
+    public String toString() {
+        return "CommonCodeGroup{" +
+                "description='" + description + '\'' +
+                ", groupCode='" + groupCode + '\'' +
+                ", name='" + name + '\'' +
+                ", isActive=" + isActive +
+                '}';
     }
 }
