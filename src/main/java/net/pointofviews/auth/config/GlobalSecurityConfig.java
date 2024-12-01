948097676339c72492f248cf15c1c167e47c11c1
kim-youngchul <kmosh1212@naver.com>
2024-12-01 21:02:57 +0900

    refactor: security debug 비활성화

--- src/main/java/net/pointofviews/auth/config/GlobalSecurityConfig.java
+++ src/main/java/net/pointofviews/auth/config/GlobalSecurityConfig.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
@@ -14,7 +13,6 @@ import org.springframework.security.web.util.matcher.RequestMatcher;
 
 @Configuration
 @RequiredArgsConstructor
-@EnableWebSecurity(debug = true)
 public class GlobalSecurityConfig {
 
     private final CorsConfig cors;
