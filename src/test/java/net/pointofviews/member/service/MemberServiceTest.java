ae9a11e8e35a0a58401e5b56ab84d467f5632654
leeseunghee00 <lsh9246@gmail.com>
2024-12-02 14:18:28 +0900

    feat: 회원 선호 장르 변경 비즈니스 로직 단위 테스트 작성

--- src/test/java/net/pointofviews/member/service/MemberServiceTest.java
+++ src/test/java/net/pointofviews/member/service/MemberServiceTest.java
@@ -1,33 +1,39 @@
 package net.pointofviews.member.service;
 
 import static org.assertj.core.api.SoftAssertions.*;
+import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.BDDMockito.*;
 
-import net.pointofviews.auth.dto.request.LoginMemberRequest;
-import net.pointofviews.auth.dto.response.LoginMemberResponse;
-import net.pointofviews.member.domain.Member;
-import net.pointofviews.member.domain.RoleType;
-import net.pointofviews.member.domain.SocialType;
-import net.pointofviews.member.dto.request.PutMemberNicknameRequest;
-import net.pointofviews.member.dto.response.PutMemberNicknameResponse;
-import net.pointofviews.member.exception.MemberException;
-import net.pointofviews.member.repository.MemberRepository;
-import net.pointofviews.member.service.impl.MemberServiceImpl;
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+import java.util.Set;
+import java.util.UUID;
 
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
-import org.junit.jupiter.api.Nested;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.http.HttpStatus;
 
-import java.time.LocalDate;
-import java.util.Optional;
-import java.util.UUID;
-
-import static org.junit.jupiter.api.Assertions.*;
-import static org.mockito.Mockito.mock;
+import net.pointofviews.auth.dto.request.LoginMemberRequest;
+import net.pointofviews.auth.dto.response.LoginMemberResponse;
+import net.pointofviews.common.domain.CodeGroupEnum;
+import net.pointofviews.member.domain.Member;
+import net.pointofviews.member.domain.MemberFavorGenre;
+import net.pointofviews.member.domain.RoleType;
+import net.pointofviews.member.domain.SocialType;
+import net.pointofviews.member.dto.request.PutMemberGenreListRequest;
+import net.pointofviews.member.dto.request.PutMemberNicknameRequest;
+import net.pointofviews.member.dto.response.PutMemberGenreListResponse;
+import net.pointofviews.member.dto.response.PutMemberNicknameResponse;
+import net.pointofviews.member.exception.MemberException;
+import net.pointofviews.member.repository.MemberFavorGenreRepository;
+import net.pointofviews.member.repository.MemberRepository;
+import net.pointofviews.member.service.impl.MemberServiceImpl;
 
 @ExtendWith(MockitoExtension.class)
 class MemberServiceTest {
@@ -37,6 +43,9 @@ class MemberServiceTest {
 
 	@Mock
 	private MemberRepository memberRepository;
+
+	@Mock
+	private MemberFavorGenreRepository memberFavorGenreRepository;
 
 	@Test
 	void signup() {
@@ -126,8 +135,161 @@ class MemberServiceTest {
 	void deleteMember() {
 	}
 
-	@Test
-	void updateGenre() {
+	@Nested
+	class UpdateGenre {
+
+		@Nested
+		class Success {
+
+			@Test
+			void 선호하는_장르_변경_시_기존_장르와_비교_후_장르_추가() {
+				// given -- 테스트의 상태 설정
+				Member member = mock(Member.class);
+				given(memberRepository.findById(any())).willReturn(Optional.of(member));
+
+				List<String> existingGenres = List.of("01", "02", "03");
+				given(memberFavorGenreRepository.findGenreCodeByMemberId(any())).willReturn(existingGenres);
+
+				List<String> newGenres = List.of("액션", "코미디", "범죄");
+				PutMemberGenreListRequest request = new PutMemberGenreListRequest(newGenres);
+				String groupCode = CodeGroupEnum.MOVIE_GENRE.getCode();
+
+				given(memberFavorGenreRepository.findGenreCodeByGenreName("액션", groupCode)).willReturn("01");
+				given(memberFavorGenreRepository.findGenreCodeByGenreName("코미디", groupCode)).willReturn("04");
+				given(memberFavorGenreRepository.findGenreCodeByGenreName("범죄", groupCode)).willReturn("05");
+
+				// when -- 테스트하고자 하는 행동
+				PutMemberGenreListResponse result = memberService.updateGenre(member, request);
+
+				// then -- 예상되는 변화 및 결과
+				assertSoftly(softly -> {
+					softly.assertThat(result.genres()).isNotNull();
+					softly.assertThat(result.genres().size()).isEqualTo(3);
+					softly.assertThat(result.genres()).isEqualTo(newGenres);
+
+					verify(memberFavorGenreRepository).deleteByMemberIdAndGenreCodeIn(any(), any());
+					verify(memberFavorGenreRepository, times(2)).save(any(MemberFavorGenre.class));
+				});
+			}
+
+			@Test
+			void 기존_장르와_선호하는_장르가_겹치지_않을_경우_추가_작업만_진행() {
+				// given -- 테스트의 상태 설정
+				Member member = mock(Member.class);
+				given(memberRepository.findById(any())).willReturn(Optional.of(member));
+
+				List<String> existingGenres = List.of("01", "02", "03");
+				given(memberFavorGenreRepository.findGenreCodeByMemberId(any())).willReturn(existingGenres);
+
+				List<String> newGenres = List.of("코미디", "범죄", "다큐멘터리");
+				PutMemberGenreListRequest request = new PutMemberGenreListRequest(newGenres);
+				String groupCode = CodeGroupEnum.MOVIE_GENRE.getCode();
+
+				given(memberFavorGenreRepository.findGenreCodeByGenreName("코미디", groupCode)).willReturn("04");
+				given(memberFavorGenreRepository.findGenreCodeByGenreName("범죄", groupCode)).willReturn("05");
+				given(memberFavorGenreRepository.findGenreCodeByGenreName("다큐멘터리", groupCode)).willReturn("06");
+
+				// when -- 테스트하고자 하는 행동
+				PutMemberGenreListResponse result = memberService.updateGenre(member, request);
+
+				// then -- 예상되는 변화 및 결과
+				assertSoftly(softly -> {
+					softly.assertThat(result.genres()).isNotNull();
+					softly.assertThat(result.genres().size()).isEqualTo(3);
+					softly.assertThat(result.genres()).isEqualTo(newGenres);
+
+					verify(memberFavorGenreRepository, never()).deleteByMemberIdAndGenreCodeIn(member.getId(), null);
+					verify(memberFavorGenreRepository, times(3)).save(any(MemberFavorGenre.class));
+				});
+			}
+
+			@Test
+			void 기존_장르와_요청된_장르가_동일한_경우_삭제작업_미수행() {
+				// given -- 테스트의 상태 설정
+				Member member = mock(Member.class);
+				given(memberRepository.findById(any())).willReturn(Optional.of(member));
+
+				// 기존 장르와 요청된 장르가 동일
+				List<String> existingGenres = List.of("01", "02", "03");
+				given(memberFavorGenreRepository.findGenreCodeByMemberId(any())).willReturn(existingGenres);
+
+				List<String> newGenres = List.of("액션", "코미디", "범죄");
+				PutMemberGenreListRequest request = new PutMemberGenreListRequest(newGenres);
+				String groupCode = CodeGroupEnum.MOVIE_GENRE.getCode();
+
+				given(memberFavorGenreRepository.findGenreCodeByGenreName("액션", groupCode)).willReturn("01");
+				given(memberFavorGenreRepository.findGenreCodeByGenreName("코미디", groupCode)).willReturn("02");
+				given(memberFavorGenreRepository.findGenreCodeByGenreName("범죄", groupCode)).willReturn("03");
+
+				// when -- 테스트하고자 하는 행동
+				PutMemberGenreListResponse result = memberService.updateGenre(member, request);
+
+				// then -- 예상되는 변화 및 결과
+				assertSoftly(softly -> {
+					softly.assertThat(result.genres()).isNotNull();
+					softly.assertThat(result.genres().size()).isEqualTo(3);
+					softly.assertThat(result.genres()).isEqualTo(newGenres);
+
+					verify(memberFavorGenreRepository, never()).deleteByMemberIdAndGenreCodeIn(any(), any());
+					verify(memberFavorGenreRepository, never()).save(any(MemberFavorGenre.class));
+				});
+			}
+
+		}
+
+		@Nested
+		class Failure {
+
+			@Test
+			void 존재하지_않는_회원_MemberException_memberNotFound_예외발생() {
+				// given -- 테스트의 상태 설정
+				Member member = mock(Member.class);
+				PutMemberGenreListRequest request = new PutMemberGenreListRequest(List.of("액션", "코미디", "범죄"));
+
+				UUID memberId = UUID.randomUUID();
+
+				given(member.getId()).willReturn(memberId);
+				given(memberRepository.findById(any())).willReturn(Optional.empty());
+
+				// when -- 테스트하고자 하는 행동
+				MemberException exception = assertThrows(MemberException.class, () ->
+					memberService.updateGenre(member, request)
+				);
+
+				// then -- 예상되는 변화 및 결과
+				assertSoftly(softly -> {
+					softly.assertThat(exception.getStatus()).isEqualTo(HttpStatus.NOT_FOUND);
+					softly.assertThat(exception.getMessage())
+						.isEqualTo(String.format("회원(Id: %s)이 존재하지 않습니다.", memberId));
+				});
+			}
+
+			@Test
+			void 유효하지_않은_장르_MemberException_memberGenreBadRequest_예외발생() {
+				// given -- 테스트의 상태 설정
+				Member member = mock(Member.class);
+				PutMemberGenreListRequest request = new PutMemberGenreListRequest(List.of("액션", "코미디", "시사교양"));
+
+				given(memberRepository.findById(any())).willReturn(Optional.of(member));
+
+				String groupCode = CodeGroupEnum.MOVIE_GENRE.getCode();
+
+				given(memberFavorGenreRepository.findGenreCodeByGenreName("액션", groupCode)).willReturn("01");
+				given(memberFavorGenreRepository.findGenreCodeByGenreName("코미디", groupCode)).willReturn("04");
+				given(memberFavorGenreRepository.findGenreCodeByGenreName("시사교양", groupCode)).willReturn(null);
+
+				// when -- 테스트하고자 하는 행동
+				MemberException exception = assertThrows(MemberException.class, () ->
+					memberService.updateGenre(member, request)
+				);
+
+				// then -- 예상되는 변화 및 결과
+				assertSoftly(softly -> {
+					softly.assertThat(exception.getStatus()).isEqualTo(HttpStatus.BAD_REQUEST);
+					softly.assertThat(exception.getMessage()).isEqualTo("잘못된 장르(Name: 시사교양)를 요청했습니다.");
+				});
+			}
+		}
 	}
 
 	@Test
@@ -209,7 +371,6 @@ class MemberServiceTest {
 				PutMemberNicknameRequest request = new PutMemberNicknameRequest("nickname");
 
 				given(memberRepository.existsByNickname(any())).willReturn(true);
-
 
 				// when -- 테스트하고자 하는 행동
 				MemberException exception = assertThrows(MemberException.class, () ->
