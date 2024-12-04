3d03ed79c91adbbce372b9c01caa45b65deb514d
leeseunghee00 <lsh9246@gmail.com>
2024-12-04 18:35:52 +0900

    refactor: 영화별 리뷰 조회 최신 순 정렬로 변경

--- src/main/java/net/pointofviews/review/repository/ReviewRepository.java
+++ src/main/java/net/pointofviews/review/repository/ReviewRepository.java
@@ -1,45 +1,45 @@
 package net.pointofviews.review.repository;
 
-import java.util.Optional;
-
+import net.pointofviews.review.domain.Review;
+import net.pointofviews.review.dto.response.ReadReviewResponse;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Slice;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
-import net.pointofviews.review.domain.Review;
-import net.pointofviews.review.dto.response.ReadReviewResponse;
+import java.util.Optional;
 
 public interface ReviewRepository extends JpaRepository<Review, Long> {
 
-	@Query(value = """
-		SELECT new net.pointofviews.review.dto.response.ReadReviewResponse(
-				r.id,
-				mv.title,
-				r.title,
-				r.contents,
-				m.nickname,
-				m.profileImage,
-				mv.poster,
-				r.createdAt,
-				(SELECT rlc.reviewLikeCount FROM ReviewLikeCount rlc WHERE rlc.review.id = r.id),
-				CASE WHEN EXISTS (SELECT 1 FROM ReviewLike rl WHERE rl.review.id = r.id AND rl.isLiked = true) THEN true ELSE false END,
-				r.isSpoiler
-		 )
-		 FROM Review r
-		 JOIN r.member m
-		 JOIN r.movie mv
-		 WHERE mv.id = :movieId
-	""")
-	Slice<ReadReviewResponse> findReviewsWithLikesByMovieId(@Param("movieId") Long movieId, Pageable pageable);
+    @Query(value = """
+            	SELECT new net.pointofviews.review.dto.response.ReadReviewResponse(
+            			r.id,
+            			mv.title,
+            			r.title,
+            			r.contents,
+            			m.nickname,
+            			m.profileImage,
+            			mv.poster,
+            			r.createdAt,
+            			(SELECT rlc.reviewLikeCount FROM ReviewLikeCount rlc WHERE rlc.review.id = r.id),
+            			CASE WHEN EXISTS (SELECT 1 FROM ReviewLike rl WHERE rl.review.id = r.id AND rl.isLiked = true) THEN true ELSE false END,
+            			r.isSpoiler
+            	 )
+            	 FROM Review r
+            	 JOIN r.member m
+            	 JOIN r.movie mv
+            	 WHERE mv.id = :movieId
+                ORDER BY r.createdAt DESC
+            """)
+    Slice<ReadReviewResponse> findReviewsWithLikesByMovieId(@Param("movieId") Long movieId, Pageable pageable);
 
-	@Query("""
-		SELECT r
-		  FROM Review r
-		  JOIN FETCH r.member m
-		  JOIN FETCH r.movie mv
-		 WHERE r.id = :reviewId
-	""")
-	Optional<Review> findReviewDetailById(@Param("reviewId") Long reviewId);
+    @Query(value = """
+            	SELECT r
+            	  FROM Review r
+            	  JOIN FETCH r.member m
+            	  JOIN FETCH r.movie mv
+            	 WHERE r.id = :reviewId
+            """)
+    Optional<Review> findReviewDetailById(@Param("reviewId") Long reviewId);
 }
