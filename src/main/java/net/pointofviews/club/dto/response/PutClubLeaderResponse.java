8ae2f2faf650319b2d377f6ac76eda58c8e518d8
ssiiin0-0 <gomee312@naver.com>
2024-12-06 12:45:24 +0900

    feat: 클럽생성, 수정, 삭제 및 클럽탈퇴, 클럽장 변경, 클럽이미지 관련 초기 설정 (api, swagger)

--- src/main/java/net/pointofviews/club/dto/response/PutClubLeaderResponse.java
+++ src/main/java/net/pointofviews/club/dto/response/PutClubLeaderResponse.java
@@ -8,8 +8,8 @@ public record PutClubLeaderResponse(
         @Schema(description = "클럽 ID")
         UUID clubId,
 
-        @Schema(description = "새로운 클럽장 ID")
-        UUID newLeaderId,
+        @Schema(description = "새로운 클럽장 이메일", example = "user@example.com")
+        String newLeaderEmail,
 
         @Schema(description = "새로운 클럽장 닉네임", example = "새클럽장")
         String newLeaderNickname
