ef08fb1660b93d38a3bf1838ff4de2551ff78b91
njm <166492522+nohzimin@users.noreply.github.com>
2024-12-06 16:27:06 +0900

    feat: 영화 검색 쿼리 수정

--- src/main/java/net/pointofviews/movie/repository/MovieRepository.java
+++ src/main/java/net/pointofviews/movie/repository/MovieRepository.java
@@ -15,25 +15,26 @@ public interface MovieRepository extends JpaRepository<Movie, Long> {
      * 검색
      */
     @Query(value = """
-    SELECT DISTINCT m.id, m.title, m.poster, 
-                    m.released AS released, 
-                    COALESCE(mlc.like_count, 0) AS likeCount, 
-                    (SELECT COUNT(*) FROM review r WHERE r.movie_id = m.id) AS reviewCount 
-    FROM movie m 
-    LEFT JOIN movie_like_count mlc ON mlc.movie_id = m.id 
-    WHERE EXISTS ( 
-        SELECT 1 
-        FROM people p 
-        JOIN movie_cast mc ON mc.people_id = p.id 
-        WHERE p.name LIKE CONCAT('%', :query, '%') 
-          AND mc.movie_id = m.id 
-    ) 
-       OR EXISTS ( 
-        SELECT 1 
-        FROM people p 
-        JOIN movie_crew mcr ON mcr.people_id = p.id 
-        WHERE p.name LIKE CONCAT('%', :query, '%') 
-          AND mcr.movie_id = m.id 
+
+    SELECT DISTINCT m.id, m.title, m.poster, m.released AS released,
+    COALESCE(mlc.like_count, 0) AS likeCount,
+                    (SELECT COUNT(*) FROM review r WHERE r.movie_id = m.id) AS reviewCount
+    FROM movie m
+    LEFT JOIN movie_like_count mlc ON mlc.movie_id = m.id
+    WHERE MATCH(m.title) AGAINST(:query IN NATURAL LANGUAGE MODE)
+    OR EXISTS (
+            SELECT 1
+                    FROM people p
+                    JOIN movie_cast mc ON mc.people_id = p.id
+                    WHERE MATCH(p.name) AGAINST(:query IN NATURAL LANGUAGE MODE)
+                    AND mc.movie_id = m.id
+    )
+    OR EXISTS (
+            SELECT 1
+                    FROM people p
+                    JOIN movie_crew mcr ON mcr.people_id = p.id
+                    WHERE MATCH(p.name) AGAINST(:query IN NATURAL LANGUAGE MODE)
+                    AND mcr.movie_id = m.id
     )
     """,
             nativeQuery = true)
