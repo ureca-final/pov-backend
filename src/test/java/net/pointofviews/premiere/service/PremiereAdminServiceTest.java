d8df349066da6a5b0f0282d0dc25c035433c51f4
leeseunghee00 <lsh9246@gmail.com>
2024-12-10 01:18:03 +0900

    feat: 관리자 시사회 삭제 비즈니스 로직 단위 테스트 작성

--- src/test/java/net/pointofviews/premiere/service/PremiereAdminServiceTest.java
+++ src/test/java/net/pointofviews/premiere/service/PremiereAdminServiceTest.java
@@ -1,5 +1,6 @@
 package net.pointofviews.premiere.service;
 
+import net.pointofviews.common.service.S3Service;
 import net.pointofviews.fixture.PremiereFixture;
 import net.pointofviews.member.domain.Member;
 import net.pointofviews.member.exception.MemberException;
@@ -39,6 +40,9 @@ class PremiereAdminServiceTest {
 
     @Mock
     private MemberRepository memberRepository;
+
+    @Mock
+    private S3Service s3Service;
 
     @Nested
     class UpdatePremiere {
@@ -120,6 +124,95 @@ class PremiereAdminServiceTest {
                 assertSoftly(softly -> {
                    softly.assertThat(exception.getStatus()).isEqualTo(HttpStatus.NOT_FOUND);
                    softly.assertThat(exception.getMessage()).isEqualTo(String.format("시사회(Id: %d)가 존재하지 않습니다.", -1L));
+                });
+            }
+        }
+    }
+
+    @Nested
+    class DeletePremiere {
+
+        @Nested
+        class Success {
+
+            @Test
+            void 이미지를_포함한_시사회_정보_삭제() {
+                // given -- 테스트의 상태 설정
+                Member admin = mock(Member.class);
+                Premiere premiere = PremiereFixture.createPremiere();
+
+                given(memberRepository.findById(any())).willReturn(Optional.of(admin));
+                given(premiereRepository.findById(any())).willReturn(Optional.of(premiere));
+
+                // when -- 테스트하고자 하는 행동
+                premiereAdminService.deletePremiere(admin, 1L);
+
+                // then -- 예상되는 변화 및 결과
+                verify(s3Service, times(1)).deleteImage(any());
+                verify(premiereRepository, times(1)).delete(any());
+            }
+
+            @Test
+            void 이미지가_null_인_시사회_정보_삭제() {
+                // given -- 테스트의 상태 설정
+                Member admin = mock(Member.class);
+                Premiere premiere = mock(Premiere.class);
+
+                given(memberRepository.findById(any())).willReturn(Optional.of(admin));
+                given(premiereRepository.findById(any())).willReturn(Optional.of(premiere));
+
+                // when -- 테스트하고자 하는 행동
+                premiereAdminService.deletePremiere(admin, 1L);
+
+                // then -- 예상되는 변화 및 결과
+                verify(s3Service, times(0)).deleteImage(any());
+                verify(premiereRepository, times(1)).delete(any());
+            }
+        }
+
+        @Nested
+        class Failure {
+
+            @Test
+            void 존재하지_않는_관리자_MemberException_memberNotFound_예외발생() {
+                // given -- 테스트의 상태 설정
+                Member admin = mock(Member.class);
+                UUID adminId = UUID.randomUUID();
+
+                given(admin.getId()).willReturn(adminId);
+                given(memberRepository.findById(any())).willReturn(Optional.empty());
+
+                // when -- 테스트하고자 하는 행동
+                MemberException exception = assertThrows(MemberException.class, () ->
+                        premiereAdminService.deletePremiere(admin, 1L)
+                );
+
+                // then -- 예상되는 변화 및 결과
+                assertSoftly(softly -> {
+                    softly.assertThat(exception.getStatus()).isEqualTo(HttpStatus.NOT_FOUND);
+                    softly.assertThat(exception.getMessage()).isEqualTo(String.format("관리자(Id: %s)가 존재하지 않습니다.", adminId));
+                    verify(premiereRepository, times(0)).findById(any());
+                });
+            }
+
+            @Test
+            void 존재하지_않는_시사회_PremiereException_premiereNotFound_예외발생() {
+                // given -- 테스트의 상태 설정
+                Member admin = mock(Member.class);
+                Premiere premiere = mock(Premiere.class);
+
+                given(memberRepository.findById(any())).willReturn(Optional.of(admin));
+                given(premiereRepository.findById(any())).willReturn(Optional.empty());
+
+                // when -- 테스트하고자 하는 행동
+                PremiereException exception = assertThrows(PremiereException.class, () ->
+                        premiereAdminService.deletePremiere(admin, -1L)
+                );
+
+                // then -- 예상되는 변화 및 결과
+                assertSoftly(softly -> {
+                    softly.assertThat(exception.getStatus()).isEqualTo(HttpStatus.NOT_FOUND);
+                    softly.assertThat(exception.getMessage()).isEqualTo(String.format("시사회(Id: %d)가 존재하지 않습니다.", -1L));
                 });
             }
         }
