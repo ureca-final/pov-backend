ca6402e95f04cb4d89fae1ea890045ff03138a05
leeseunghee00 <lsh9246@gmail.com>
2024-12-10 02:18:51 +0900

    feat: 관리자 시사회 상세 조회 비즈니스 로직 단위 테스트 작성

--- src/test/java/net/pointofviews/premiere/service/PremiereAdminServiceTest.java
+++ src/test/java/net/pointofviews/premiere/service/PremiereAdminServiceTest.java
@@ -7,6 +7,7 @@ import net.pointofviews.member.exception.MemberException;
 import net.pointofviews.member.repository.MemberRepository;
 import net.pointofviews.premiere.domain.Premiere;
 import net.pointofviews.premiere.dto.request.PremiereRequest;
+import net.pointofviews.premiere.dto.response.ReadDetailPremiereResponse;
 import net.pointofviews.premiere.exception.PremiereException;
 import net.pointofviews.premiere.repository.PremiereRepository;
 import net.pointofviews.premiere.service.imple.PremiereAdminServiceImpl;
@@ -122,8 +123,8 @@ class PremiereAdminServiceTest {
 
                 // then -- 예상되는 변화 및 결과
                 assertSoftly(softly -> {
-                   softly.assertThat(exception.getStatus()).isEqualTo(HttpStatus.NOT_FOUND);
-                   softly.assertThat(exception.getMessage()).isEqualTo(String.format("시사회(Id: %d)가 존재하지 않습니다.", -1L));
+                    softly.assertThat(exception.getStatus()).isEqualTo(HttpStatus.NOT_FOUND);
+                    softly.assertThat(exception.getMessage()).isEqualTo(String.format("시사회(Id: %d)가 존재하지 않습니다.", -1L));
                 });
             }
         }
@@ -199,7 +200,6 @@ class PremiereAdminServiceTest {
             void 존재하지_않는_시사회_PremiereException_premiereNotFound_예외발생() {
                 // given -- 테스트의 상태 설정
                 Member admin = mock(Member.class);
-                Premiere premiere = mock(Premiere.class);
 
                 given(memberRepository.findById(any())).willReturn(Optional.of(admin));
                 given(premiereRepository.findById(any())).willReturn(Optional.empty());
@@ -207,6 +207,82 @@ class PremiereAdminServiceTest {
                 // when -- 테스트하고자 하는 행동
                 PremiereException exception = assertThrows(PremiereException.class, () ->
                         premiereAdminService.deletePremiere(admin, -1L)
+                );
+
+                // then -- 예상되는 변화 및 결과
+                assertSoftly(softly -> {
+                    softly.assertThat(exception.getStatus()).isEqualTo(HttpStatus.NOT_FOUND);
+                    softly.assertThat(exception.getMessage()).isEqualTo(String.format("시사회(Id: %d)가 존재하지 않습니다.", -1L));
+                });
+            }
+        }
+    }
+
+    @Nested
+    class FindPremiereDetail {
+
+        @Nested
+        class Success {
+
+            @Test
+            void 시사회_상세_정보_조회() {
+                // given -- 테스트의 상태 설정
+                Member admin = mock(Member.class);
+                Premiere premiere = PremiereFixture.createPremiere();
+
+                given(memberRepository.findById(any())).willReturn(Optional.of(admin));
+                given(premiereRepository.findById(any())).willReturn(Optional.of(premiere));
+
+                // when -- 테스트하고자 하는 행동
+                ReadDetailPremiereResponse result = premiereAdminService.findPremiereDetail(admin, 1L);
+
+                // then -- 예상되는 변화 및 결과
+                assertSoftly(softly -> {
+                    softly.assertThat(result).isNotNull();
+                    softly.assertThat(result.title()).isEqualTo(premiere.getTitle());
+                    softly.assertThat(result.startAt()).isEqualTo(premiere.getStartAt());
+                    softly.assertThat(result.endAt()).isEqualTo(premiere.getEndAt());
+                    softly.assertThat(result.eventImage()).isEqualTo(premiere.getEventImage());
+                });
+            }
+        }
+
+        @Nested
+        class Failure {
+
+            @Test
+            void 존재하지_않는_관리자_MemberException_memberNotFound_예외발생() {
+                // given -- 테스트의 상태 설정
+                Member admin = mock(Member.class);
+                UUID adminId = UUID.randomUUID();
+
+                given(admin.getId()).willReturn(adminId);
+                given(memberRepository.findById(any())).willReturn(Optional.empty());
+
+                // when -- 테스트하고자 하는 행동
+                MemberException exception = assertThrows(MemberException.class, () ->
+                        premiereAdminService.findPremiereDetail(admin, 1L)
+                );
+
+                // then -- 예상되는 변화 및 결과
+                assertSoftly(softly -> {
+                    softly.assertThat(exception.getStatus()).isEqualTo(HttpStatus.NOT_FOUND);
+                    softly.assertThat(exception.getMessage()).isEqualTo(String.format("관리자(Id: %s)가 존재하지 않습니다.", adminId));
+                    verify(premiereRepository, times(0)).findById(any());
+                });
+            }
+
+            @Test
+            void 존재하지_않는_시사회_PremiereException_premiereNotFound_예외발생() {
+                // given -- 테스트의 상태 설정
+                Member admin = mock(Member.class);
+
+                given(memberRepository.findById(any())).willReturn(Optional.of(admin));
+                given(premiereRepository.findById(any())).willReturn(Optional.empty());
+
+                // when -- 테스트하고자 하는 행동
+                PremiereException exception = assertThrows(PremiereException.class, () ->
+                        premiereAdminService.findPremiereDetail(admin, -1L)
                 );
 
                 // then -- 예상되는 변화 및 결과
