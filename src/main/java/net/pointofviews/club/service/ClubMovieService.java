86569700cb12e95e7715e82f7fefd43a50fe1dca
njm <166492522+nohzimin@users.noreply.github.com>
2024-12-13 02:25:59 +0900

    feat: 내 클럽에 영화 북마크 기능 구현

--- src/main/java/net/pointofviews/club/service/ClubMovieService.java
+++ src/main/java/net/pointofviews/club/service/ClubMovieService.java
@@ -1,10 +1,13 @@
 package net.pointofviews.club.service;
 
 import net.pointofviews.club.dto.response.ReadClubMoviesListResponse;
+import net.pointofviews.member.domain.Member;
 import org.springframework.data.domain.Pageable;
 
 import java.util.UUID;
 
 public interface ClubMovieService {
     ReadClubMoviesListResponse readClubMovies(UUID clubId, Pageable pageable);
+
+    void saveMovieToMyClub(Long movieId, UUID clubId);
 }
