311dc07251e3cf5167a2ef982d081278eb88a16f
leeseunghee00 <lsh9246@gmail.com>
2024-12-13 10:08:04 +0900

    feat: Redisson 을 이용한 Distribute Lock 동시성 처리

---
+++ src/main/java/net/pointofviews/common/lock/CustomSpringELParser.java
@@ -0,0 +1,19 @@
+package net.pointofviews.common.lock;
+
+import org.springframework.expression.ExpressionParser;
+import org.springframework.expression.spel.standard.SpelExpressionParser;
+import org.springframework.expression.spel.support.StandardEvaluationContext;
+
+public class CustomSpringELParser {
+
+    public static Object getDynamicValue(String[] parameterNames, Object[] args, String key) {
+        ExpressionParser parser = new SpelExpressionParser();
+        StandardEvaluationContext context = new StandardEvaluationContext();
+
+        for (int i = 0; i < parameterNames.length; i++) {
+            context.setVariable(parameterNames[i], args[i]);
+        }
+
+        return parser.parseExpression(key).getValue(context);
+    }
+}
