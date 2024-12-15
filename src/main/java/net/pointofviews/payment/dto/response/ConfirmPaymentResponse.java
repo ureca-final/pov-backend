3ca71f23a38d0caa257a49b9fb75263f1e84247b
leeseunghee00 <lsh9246@gmail.com>
2024-12-15 21:22:07 +0900

    fix: 결제 승인 후 반환된 응답값 중 필요한 응답값만 받도록 수정

--- src/main/java/net/pointofviews/payment/dto/response/ConfirmPaymentResponse.java
+++ src/main/java/net/pointofviews/payment/dto/response/ConfirmPaymentResponse.java
@@ -1,7 +1,10 @@
 package net.pointofviews.payment.dto.response;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
 import java.time.LocalDateTime;
 
+@JsonIgnoreProperties(ignoreUnknown = true)
 public record ConfirmPaymentResponse(
         String paymentKey,
         String orderId,
