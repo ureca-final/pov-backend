2f1e5fd835e733b373dc0f7286bcce4a99cc02da
leeseunghee00 <lsh9246@gmail.com>
2024-12-16 11:04:50 +0900

    refactor: 로그 메시지 수정 및 예외 조건 추가

--- src/main/java/net/pointofviews/payment/repository/TempPaymentRepository.java
+++ src/main/java/net/pointofviews/payment/repository/TempPaymentRepository.java
@@ -4,8 +4,13 @@ import net.pointofviews.payment.domain.TempPayment;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
+import java.util.UUID;
 
 public interface TempPaymentRepository extends JpaRepository<TempPayment, Long> {
 
     Optional<TempPayment> findByOrderId(String orderId);
+
+    boolean existsByMemberIdAndOrderId(UUID memberId, String orderId);
+
+    void deleteByOrderId(String orderId);
 }
