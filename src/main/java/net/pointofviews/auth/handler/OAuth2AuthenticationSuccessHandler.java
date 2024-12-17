bd0bbadd61a2998179cc2163c9be431f6ff6552b
kim-youngchul <kmosh1212@naver.com>
2024-12-18 02:54:15 +0900

    feat: naver OAuth2 시 프로필 정보만 반환

--- src/main/java/net/pointofviews/auth/handler/OAuth2AuthenticationSuccessHandler.java
+++ src/main/java/net/pointofviews/auth/handler/OAuth2AuthenticationSuccessHandler.java
@@ -6,7 +6,6 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import net.pointofviews.auth.dto.MemberDetailsDto;
 import net.pointofviews.member.domain.Member;
-import net.pointofviews.member.repository.MemberFavorGenreRepository;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.oauth2.client.authentication.OAuth2AuthenticationToken;
@@ -24,8 +23,6 @@ public class OAuth2AuthenticationSuccessHandler implements AuthenticationSuccess
     @Value("${oauth.baseurl}")
     private String redirectBaseUrl;
 
-    private final MemberFavorGenreRepository memberFavorGenreRepository;
-
     @Override
     public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException {
         try {
@@ -33,15 +30,13 @@ public class OAuth2AuthenticationSuccessHandler implements AuthenticationSuccess
             OAuth2AuthenticationToken authToken = (OAuth2AuthenticationToken) authentication;
             OAuth2User oAuth2User = authToken.getPrincipal();
 
-
             if (oAuth2User instanceof MemberDetailsDto memberDetails) {
                 Member member = memberDetails.member();
-                boolean exists = memberFavorGenreRepository.existsByMemberId(member.getId());
 
                 String socialType = member.getSocialType().toString().toLowerCase();
 
                 response.sendRedirect(redirectBaseUrl + "/oauth/" + socialType + "/callback?email=" + member.getEmail() +
-                        "&image=" + member.getProfileImage() + "&exists=" + exists);
+                        "&image=" + member.getProfileImage());
             }
         } catch (Exception e) {
             response.setStatus(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
