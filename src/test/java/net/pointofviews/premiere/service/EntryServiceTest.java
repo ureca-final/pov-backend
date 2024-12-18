407e6e8253694986cd7366a3bfd3e90596b14023
leeseunghee00 <lsh9246@gmail.com>
2024-12-19 00:58:15 +0900

    feat: 시사회 응모 분산락 동시성 테스트 추가

--- src/test/java/net/pointofviews/premiere/service/EntryServiceTest.java
+++ src/test/java/net/pointofviews/premiere/service/EntryServiceTest.java
@@ -22,6 +22,10 @@ import org.springframework.http.HttpStatus;
 
 import java.util.Optional;
 import java.util.UUID;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.atomic.AtomicInteger;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.SoftAssertions.assertSoftly;
@@ -51,6 +55,41 @@ class EntryServiceTest {
 
         @Nested
         class Success {
+
+            @Test
+            void 시사회_선착순_100명_분산락_동시성_100명_응모_성공() throws InterruptedException {
+                // given -- 테스트의 상태 설정
+                int numThreads = 100;
+                ExecutorService executorService = Executors.newFixedThreadPool(numThreads);
+                CountDownLatch latch = new CountDownLatch(numThreads);
+
+                Premiere premiere = PremiereFixture.createPremiere(100);
+                given(premiereRepository.findById(any())).willReturn(Optional.of(premiere));
+
+                AtomicInteger successCount = new AtomicInteger(0);
+
+                // when -- 테스트하고자 하는 행동
+                for (int i = 0; i < numThreads; i++) {
+                    Member member = mock(Member.class);
+                    given(memberRepository.findById(any())).willReturn(Optional.of(member));
+
+                    CreateEntryRequest request = new CreateEntryRequest(1, premiere.getAmount());
+
+                    executorService.submit(() -> {
+                        try {
+                            entryService.saveEntry(member, 1L, request);
+                            successCount.incrementAndGet();
+                        } finally {
+                            latch.countDown();
+                        }
+                    });
+                }
+
+                latch.await();
+
+                // then -- 예상되는 변화 및 결과
+                assertThat(successCount.get()).isEqualTo(numThreads);
+            }
 
             @Test
             void 시사회_현재_남은_수량_보다_적은_수로_응모할_경우_응모_성공() {
