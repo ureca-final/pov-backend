ad336a3a3f2fc5da5082e04cc08e732c9944fe59
leeseunghee00 <lsh9246@gmail.com>
2024-12-19 21:04:14 +0900

    test: 테스트를 위해 분산락을 적용하지 않은 응모 로직 추가

--- src/main/java/net/pointofviews/premiere/service/impl/EntryServiceImpl.java
+++ src/main/java/net/pointofviews/premiere/service/impl/EntryServiceImpl.java
@@ -39,9 +39,55 @@ public class EntryServiceImpl implements EntryService {
     private final TempPaymentRepository tempPaymentRepository;
 
     @Override
-    @Transactional
     @DistributeLock(key = "#premiereId")
     public CreateEntryResponse saveEntry(Member loginMember, Long premiereId, CreateEntryRequest request) {
+
+        Member member = memberRepository.findById(loginMember.getId())
+                .orElseThrow(() -> memberNotFound(loginMember.getId()));
+
+        Premiere premiere = premiereRepository.findById(premiereId)
+                .orElseThrow(() -> premiereNotFound(premiereId));
+
+        Long currQuantity = entryRepository.countEntriesByPremiereId(premiereId);
+        if (currQuantity + request.quantity() > premiere.getMaxQuantity()) {
+            log.warn("[응모오류] 수량 초과 - 시사회 수량: {}, 초과된 시사회 수량: {}",
+                    premiere.getMaxQuantity(),
+                    currQuantity + request.quantity() - premiere.getMaxQuantity());
+
+            throw quantityExceeded();
+        }
+
+        if (entryRepository.existsEntryByMemberIdAndPremiereId(member.getId(), premiereId)) {
+            log.warn("[응모오류] 응모 중복 - 회원 ID: {}, 응모한 시사회 ID: {}", member.getId(), premiereId);
+            throw duplicateEntry();
+        }
+
+        int requestTotalAmount = request.quantity() * request.amount();
+        int premiereTotalAmount = request.quantity() * premiere.getAmount();
+
+        if (requestTotalAmount != premiereTotalAmount) {
+            log.warn("[응모오류] 금액 불일치 - 요청한 수량의 총 금액: {}, 실제 총 금액: {}", requestTotalAmount, premiereTotalAmount);
+            throw entryBadRequest();
+        }
+
+        String orderId = UUID.randomUUID().toString();
+
+        Entry entry = Entry.builder()
+                .member(member)
+                .premiere(premiere)
+                .orderId(orderId)
+                .quantity(request.quantity())
+                .amount(requestTotalAmount)
+                .build();
+
+        entryRepository.save(entry);
+
+        return new CreateEntryResponse(entry.getOrderId());
+    }
+
+    @Override
+    @Transactional
+    public CreateEntryResponse saveEntry2(Member loginMember, Long premiereId, CreateEntryRequest request) {
 
         Member member = memberRepository.findById(loginMember.getId())
                 .orElseThrow(() -> memberNotFound(loginMember.getId()));
